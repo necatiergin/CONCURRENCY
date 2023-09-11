6c4ba63d497d14a7085e97a5e18f9d32e7c8141d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-11 19:20:41 +0300

    Create running_member_function.cpp

---
+++ thread/running_member_function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+
+class Nec
+{
+public:
+    void func(int x)
+    {
+        std::cout << x << '\n';
+    }
+};
+
+int main()
+{
+    Nec nec;
+    // Create and execute the thread
+    std::thread t{ &Nec::func, &nec, 13 };
+
+    t.join();
+}
