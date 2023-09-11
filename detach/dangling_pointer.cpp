7733349e12d62e3746d7bbc3fbca7798feb62a3d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-11 19:26:32 +0300

    Create dangling_pointer.cpp

---
+++ detach/dangling_pointer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <numeric>
+
+void fworker(const std::vector<int>* vp)
+{
+	std::cout << std::accumulate(vp->begin(), vp->end(), 0) << '\n';
+}
+
+
+void thread_starter()
+{
+	std::vector<int> ivec(1'000'000, 5);
+	std::thread t{ fworker, &ivec}; //dikkat yerel nesne adresi
+	t.detach();
+}
+
+int main()
+{
+	thread_starter();
+	std::this_thread::sleep_for(std::chrono::seconds{ 5 });
+}
