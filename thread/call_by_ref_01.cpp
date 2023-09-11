46fa0ed71fb339814950dd76ef3fe6931e505c08
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-11 19:17:20 +0300

    Create call_by_ref_01.cpp

---
+++ thread/call_by_ref_01.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <functional>
+
+void print(const std::vector<int>& vec)
+{
+	for (auto i : vec)
+		std::cout << i << ' ';
+	std::cout << '\n;
+}
+
+
+int main()
+{
+	using namespace std;
+
+	vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7 };
+	thread tx{ print, ivec }; //call by value
+	tx.join();
+	thread ty{ print, ref(ivec) }; //call by reference
+	ty.join();
+}
