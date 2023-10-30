171ddde2e6d64a3a4243bd39d86d5f6534ddaf47
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-10-30 16:53:14 +0300

    Update thread_callables.cpp

--- thread/thread_callables.cpp
+++ thread/thread_callables.cpp
@@ -3,58 +3,59 @@
 
 class Functor {
 public:
-	void operator()(int n)const
+	void operator()(int first, int last)const
 	{
-		for (int i = 0; i < n; ++i)
-			std::cout << i << " ";
-		std::cout << std::endl;
+		for (int i = first; i < last; ++i)
+			std::cout << i << ' ';
+		
+		std::cout << '\n';
 	}
 };
 
 class Myclass {
 public:
-	static void sprintn(int n)
+	static void sprintn(int first, int last)
 	{
-		for (int i = 0; i < n; ++i)
+		for (int i = first; i < last; ++i)
 			std::cout << i << ' ';
 		std::cout << '\n';
 	}
 
-	void printn(int n)const
+	void printn(int first, int last) const
 	{
-		for (int i = 0; i < n; ++i)
+		for (int i = first; i < last; ++i)
 			std::cout << i << ' ';
 		std::cout << '\n';
 	}
 
 };
 
-void printn(int n)
+void printn(int first, int last)
 {
-	for (int i = 0; i < n; ++i)
+	for (int i = first; i < last; ++i)
 		std::cout << i << ' ';
 	std::cout << '\n';
 }
 
 int main()
 {
-	std::thread t1{ printn, 10 };
+	std::thread t1{ printn, 10, 20 };
 	t1.join();
-	
-	std::thread t2{ Functor{}, 10 };
+
+	std::thread t2{ Functor{}, 20, 30 };
 	t2.join();
 
-	std::thread t3{ [](int n) {
-		for (int i = 0; i < n; ++i)
+	std::thread t3{ [](int first, int last) {
+		for (int i = first; i < last; ++i)
 			std::cout << i << ' ';
-		std::cout << std::endl;
-	}, 10};
+		std::cout << '\n';
+	}, 30, 40 };
 	t3.join();
 
-	std::thread t4{ Myclass::sprintn, 10 };
+	std::thread t4{ Myclass::sprintn, 40, 50 };
 	t4.join();
 
 	Myclass m;
-	std::thread t5{ &Myclass::printn, m, 10 };
+	std::thread t5{ &Myclass::printn, m, 50, 60 };
 	t5.join();
 }
