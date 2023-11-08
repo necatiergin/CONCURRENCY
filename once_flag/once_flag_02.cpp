6340dfac18c9c9829f29ebebc8364127c8206a63
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-08 16:20:53 +0300

    Create once_flag_02.cpp

---
+++ once_flag/once_flag_02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+std::once_flag gflag;
+
+void func(int id)
+{
+	std::call_once(gflag, [id]() {
+		// This function will be invoked only once.
+		std::cout << "first: " << id << '\n';
+	});
+}
+
+int main()
+{
+	std::vector<std::jthread> tvec;
+	
+	for (int i{}; i < 12; ++i) {
+		tvec.emplace_back(func, i);
+	}
+}
