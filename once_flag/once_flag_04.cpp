403458de24a5fdd5cab95bc3da7631fb3ba6f066
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-08 16:23:02 +0300

    Create once_flag_04.cpp

---
+++ once_flag/once_flag_04.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <mutex>
+#include <syncstream>
+#include <vector>
+#include <fstream>
+
+class Singleton {
+public:
+	Singleton(const Singleton&) = delete;
+	Singleton & operator=(const Singleton&) = delete;
+	
+	static Singleton* get_instance() 
+	{
+		call_once(m_init_flag, Singleton::init);
+
+		return m_instance;
+	}
+	
+	static void init()
+	{
+		m_instance = new Singleton();
+	}
+private:
+	static std::once_flag m_init_flag;
+	static Singleton* m_instance;
+	Singleton() = default;
+};
+
+Singleton* Singleton::m_instance{};
+std::once_flag Singleton::m_init_flag;
+
+
+void func()
+{
+	std::osyncstream{std::cout} << Singleton::get_instance() << '\n';
+}
+
+int main() 
+{
+	std::vector<std::thread> tvec;
+	for (int i = 0; i < 100; ++i) {
+		tvec.emplace_back(func);
+	}
+
+	for (auto& th : tvec)
+		th.join();
+}
