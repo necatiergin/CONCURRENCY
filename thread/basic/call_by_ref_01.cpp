13931491c95784223531c6f19324bef31c87b402
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-13 16:21:26 +0300

    Update and rename thread/call_by_ref_01.cpp to thread/basic/call_by_ref_01.cpp

--- thread/basic/call_by_ref_01.cpp
+++ thread/basic/call_by_ref_01.cpp
@@ -16,8 +16,8 @@ int main()
 	using namespace std;
 
 	vector ivec{ 1, 2, 3, 4, 5, 6, 7 };
-	thread tx{ print, ivec }; //call by value
-	tx.join();
+	//thread tx{ print, ivec }; //wrong - call by value
+	//tx.join();
 	thread ty{ print, ref(ivec) }; //call by reference
 	ty.join();
 }
