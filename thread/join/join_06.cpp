3f46d514b292729a7f6b1f2a51d1077d9fb38e8e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-16 21:14:39 +0300

    Update join_06.cpp

--- thread/join/join_06.cpp
+++ thread/join/join_06.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <thread>
 #include <syncstream>
+#include <vector>
 
 void cprint(char c)
 {
 	using namespace std::literals;
-	for (int i = 0; i < 1000; ++i)
+	for (int i = 0; i < 100; ++i)
 	{
 		std::cout << c;
 		std::this_thread::sleep_for(50ms);
@@ -14,14 +15,13 @@ void cprint(char c)
 
 int main()
 {
-	std::vector<std::thread> ta;
-	
-	for (int i{}; auto & t : ta)
-	{
-		ta.push_back(std::thread{ cprint,i + 'A' });
+	std::vector<std::thread> tvec;
+
+	for (int i = 0; i < 10; ++i) {
+		tvec.push_back(std::thread{ cprint, i++ + 'A' });
 	}
 
-	for (auto& t : ta)
+	for (auto& t : tvec)
 	{
 		t.join();
 	}
