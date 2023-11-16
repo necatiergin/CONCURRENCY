f07ef6d9ec037a1749518ff8307b29475327b03d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-16 17:57:51 +0300

    Create deadlock_02.cpp

---
+++ mutex/deadlock/deadlock_02.cpp
@@ -0,0 +1,37 @@
+#include <mutex>
+#include <iostream>
+
+std::mutex a_mtx;
+std::mutex b_mtx;
+
+void foo()
+{
+	using namespace std::literals;
+	a_mtx.lock();
+	std::this_thread::sleep_for(100ms);
+	b_mtx.lock();
+	std::cout << "func()" << std::endl;
+	a_mtx.unlock();
+	b_mtx.unlock();
+}
+
+void bar()
+{
+	using namespace std::literals;
+	b_mtx.lock();
+	std::this_thread::sleep_for(100ms);
+	a_mtx.lock();
+	std::cout << "bar()" << std::endl;
+	b_mtx.unlock();
+	a_mtx.unlock();
+}
+
+int main()
+{
+	std::thread t1{ foo };
+	std::thread t2{ bar };
+	t1.join();
+	t2.join();
+}
+
+Deadlock var
