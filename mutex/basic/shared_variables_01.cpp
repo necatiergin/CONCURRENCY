13456207238f4f3c602dc286ce58563f674d23bc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-16 16:01:59 +0300

    Create shared_variables_01.cpp

---
+++ mutex/basic/shared_variables_01.cpp
@@ -0,0 +1,29 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+unsigned long long counter = 0;
+
+void func()
+{
+	for (unsigned long long i = 0; i < 1'000'000ull; ++i) {
+		++counter;
+	}
+}
+
+int main()
+{
+	std::thread t1(func);
+	std::thread t2(func);
+	std::thread t3(func);
+	std::thread t4(func);
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+	std::cout << counter << '\n';
+}
+
+Output
+------
+1372864
