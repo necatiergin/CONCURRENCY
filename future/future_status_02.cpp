e0d1cf0c77268718c32ad1920d3aa52c88def865
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-16 17:40:35 +0300

    Create future_status_02.cpp

---
+++ future/future_status_02.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <future>
+
+using namespace std::literals;
+
+void func(std::promise<int> x)
+{
+	std::this_thread::sleep_for(8s);
+	x.set_value(25);
+}
+
+
+int main()
+{
+	std::promise<int> prom;
+	std::future<int> ftr = prom.get_future();
+	std::thread tx{ func,std::move(prom) };
+	std::future_status status{};
+
+	do {
+
+		std::cout << "before... Some work\n";
+
+		status = ftr.wait_for(900ms);
+
+		std::cout << "after... Some work\n";
+
+	} while (status != std::future_status::ready);
+
+	std::cout << "value is : " << ftr.get() << '\n';
+	tx.join();
+}
