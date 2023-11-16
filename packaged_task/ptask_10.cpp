abe9163db8a5234b1dd1bbab163c29449002dc2b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-16 17:42:40 +0300

    Create ptask_10.cpp

---
+++ packaged_task/ptask_10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <future>
+#include <thread>
+
+int main()
+{
+	using ftype = int(int, int);
+	std::packaged_task<ftype> pt_x;
+	std::packaged_task<ftype> pt_y([](int x, int y) {return x * x + y * y; });
+
+	// pt_x = pt_y; Syntax error
+
+	pt_x = std::move(pt_y);
+	std::future<int> ftr = pt_x.get_future();
+	std::thread{ std::move(pt_x),4,6 }.detach();
+	std::cout << ftr.get();
+}
