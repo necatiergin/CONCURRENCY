7312b6a25fabb6fc49fcc303ad0e330baf28f8c2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-16 17:44:41 +0300

    Create timed_mutex_01.cpp

---
+++ mutex/timed_mutex_01.cpp
@@ -0,0 +1,32 @@
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+int gcounter{};
+std::timed_mutex mtx;
+
+void increment(int i)
+{
+	using namespace std::literals;
+
+	if (mtx.try_lock_for(50ms)) {
+		++gcounter;
+		std::this_thread::sleep_for(10ms);
+		std::cout << "thread : " << i << " kritik bolgeye girdi\n";
+		mtx.unlock();
+	}
+	else
+		std::cout << "thread  " << i << " kritik bolgeye giremedi\n";
+}
+
+int main()
+{
+	std::thread t1{ increment, 1 };
+	std::thread t2{ increment, 2 };
+
+	t1.join();
+	t2.join();
+
+	std::cout << "gcounter = " << gcounter << "\n";
+}
