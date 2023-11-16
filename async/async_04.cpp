a417f537f5d8c8a6367757b69ef1e5c3341f7933
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-16 17:37:27 +0300

    Create async_04.cpp

---
+++ async/async_04.cpp
@@ -0,0 +1,45 @@
+#include <future>
+#include <iostream>
+
+int foo()
+{
+	return 1;
+}
+
+int main()
+{
+	auto ftr = std::async(foo);
+	if (ftr.valid())
+	{
+		std::cout << "future is valid, get function can be called\n";
+	}
+	else
+	{
+		std::cout << "future is invalid, get function cannot be called\n";
+	}
+
+	auto ival = ftr.get();
+	std::cout << "ival = " << ival << '\n';
+	if (ftr.valid())
+	{
+		std::cout << "future is valid, get function can be called\n";
+	}
+	else
+	{
+		std::cout << "future is invalid, get function cannot be called\n";
+	}									  
+	try {
+		auto i = ftr.get();
+	}
+	catch (const std::future_error& ex)
+	{
+		std::cout << "exception caught : " << ex.what() << '\n';
+	}
+}
+
+Output
+------
+future is valid, get function can be called
+ival = 1
+future is invalid, get function cannot be called
+exception caught : no state
