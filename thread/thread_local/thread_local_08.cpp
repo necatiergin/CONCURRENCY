3b5d6ec2c0e2f96fd06e6a6afb1a98e51fd682c2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-18 23:06:14 +0300

    Create thread_local_08.cpp

---
+++ thread/thread_local/thread_local_08.cpp
@@ -0,0 +1,23 @@
+#include <random>
+#include <iostream>
+
+thread_local std::mt19937 eng{ 454255u };
+
+void foo()
+{
+	std::uniform_int_distribution dist{ 10, 99 };
+
+	for (int i = 0; i < 10; ++i) {
+		std::cout << dist(eng) << ' ';
+	}
+}
+
+
+int main()
+{
+	std::thread t1{ foo };
+	t1.join();
+	std::cout << '\n';
+	std::thread t2{ foo };
+	t2.join();
+}
