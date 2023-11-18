c11f435b9b47a0bbc15e6981b1c6c0b2d28c3d4c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-18 05:40:32 +0300

    Create stop_source_01.cpp

---
+++ thread/jthread/stop_source_01.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+
+int main()
+{
+    using namespace std::literals::chrono_literals;
+
+    std::stop_source st_src;
+
+    std::thread 
+        foo([stoken = st_src.get_token()]() {
+        while (!stoken.stop_requested()) {
+            std::cout.put('*');
+            std::this_thread::sleep_for(50ms);
+        }
+            });
+
+    std::thread
+        bar([stoken = st_src.get_token()]() {
+        while (!stoken.stop_requested()) {
+            std::cout.put('.');
+            std::this_thread::sleep_for(1s);
+        }
+            });
+
+    std::this_thread::sleep_for(5s);
+
+    st_src.request_stop();
+    std::cout << "\nstopped\n";
+    
+    foo.join();
+    bar.join();
+}
