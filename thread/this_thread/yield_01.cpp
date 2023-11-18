0b540e65c3d7b29e97d090b8b4b8fb5b75a37b78
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-18 07:48:27 +0300

    Update yield_01.cpp

--- thread/this_thread/yield_01.cpp
+++ thread/this_thread/yield_01.cpp
@@ -4,26 +4,26 @@
 
 std::atomic<bool> ga_start{ false };
 
-void func(char id) 
+void func(char id)
 {
 	using namespace std::chrono;
-	while (!ga_start) 
-	{             
+	while (!ga_start)
+	{
 		std::this_thread::yield();
 	}
-	std::this_thread::sleep_for(500ms);
+	//std::this_thread::sleep_for(500ms);
 	std::cout << id;
 }
 
 int main()
 {
 	std::thread ar_t[26];
-	
-	for (char i{'A'}; auto & t : ar_t)
+
+	for (char i{ 'A' }; auto & t : ar_t)
 		t = std::thread(func, i++);
 
-	ga_start = true;               
+	ga_start = true;
 
-	for (auto& t : ar_t) 
+	for (auto& t : ar_t)
 		t.join();
 }
