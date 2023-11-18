bff42cd2164682393d7a30b8ef86be8b7c5b89ba
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-18 08:25:44 +0300

    Update jt_02.cpp

--- thread/jthread/jt_02.cpp
+++ thread/jthread/jt_02.cpp
@@ -2,23 +2,25 @@
 
 #include <thread>
 #include <utility>
+#include <stdexcept>
+
 
 class scoped_thread
 {
     std::thread t;
 public:
-    explicit scoped_thread(std::thread t_):
+    explicit scoped_thread(std::thread t_) :
         t(std::move(t_))
     {
-        if(!t.joinable())
+        if (!t.joinable())
             throw std::logic_error("No thread");
     }
     ~scoped_thread()
     {
         t.join();
     }
-    scoped_thread(scoped_thread const&)=delete;
-    scoped_thread& operator=(scoped_thread const&)=delete;
+    scoped_thread(scoped_thread const&) = delete;
+    scoped_thread& operator=(scoped_thread const&) = delete;
 };
 
 void do_something(int& i)
@@ -30,11 +32,11 @@ struct func
 {
     int& i;
 
-    func(int& i_):i(i_){}
+    func(int& i_) :i(i_) {}
 
     void operator()()
     {
-        for(unsigned j=0;j<1000000;++j)
+        for (unsigned j = 0; j < 1000000; ++j)
         {
             do_something(i);
         }
@@ -48,7 +50,7 @@ void f()
 {
     int some_local_state;
     scoped_thread t(std::thread(func(some_local_state)));
-        
+
     do_something_in_current_thread();
 }
 
