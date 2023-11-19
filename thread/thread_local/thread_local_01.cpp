748453938aefc7dd897a4c7a901fa43951eca06c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-19 08:55:30 +0300

    Update thread_local_01.cpp

--- thread/thread_local/thread_local_01.cpp
+++ thread/thread_local/thread_local_01.cpp
@@ -1,26 +1,31 @@
-#include <iostream>
-#include <syncstream>
-#include <string>
 #include <thread>
-#include <mutex>
+#include <string>
+#include <syncstream>
+#include <iostream>
+
 
 thread_local int tval{ 0 };
 
 void func(const std::string& thread_name)
 {
     ++tval; //senkronizasyon gerekmiyor
-    std::osyncstream{std::cout} << "tval in thread " << thread_name << " is " << tval << "\n";
+    std::osyncstream{ std::cout } << "tval in thread " << thread_name << " is " << tval << "\n";
 }
 
 int main()
 {
-    std::thread tx(func, "a");
-    std::thread ty(func, "b");
+    std::thread ta(func, "a");
+    std::thread tb(func, "b");
+    std::thread tc(func, "c");
+    std::thread td(func, "d");
 
     {
         std::osyncstream{ std::cout } << "tval in main: " << tval << '\n';
     }
 
-    tx.join();
-    ty.join();
+    ta.join();
+    tb.join();
+    tc.join();
+    td.join();
 }
+
