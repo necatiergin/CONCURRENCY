4c1cd1e853deec255b893aa9e7034af2aadff589
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-22 17:58:04 +0300

    Update once_flag_02.cpp

--- once_flag/once_flag_02.cpp
+++ once_flag/once_flag_02.cpp
@@ -10,13 +10,14 @@ void func(int id)
 	std::call_once(gflag, [id]() {
 		// This function will be invoked only once.
 		std::cout << "first: " << id << '\n';
-	});
+		});
 }
 
 int main()
 {
 	std::vector<std::jthread> tvec;
-	
+	tvec.reserve(16);
+
 	for (int i{}; i < 12; ++i) {
 		tvec.emplace_back(func, i);
 	}
