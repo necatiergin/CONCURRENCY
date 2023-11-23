c69a117297d4ec34af2d782744b0623dd3528410
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-23 20:43:34 +0300

    Update cv_02.cpp

--- condition_variable/cv_02.cpp
+++ condition_variable/cv_02.cpp
@@ -5,12 +5,12 @@
 int shared_variable{};
 std::mutex mtx;
 
+using namespace std::literals;
+
 void producer()
 {
-	using namespace std::literals;
-
 	std::this_thread::sleep_for(1000ms);
-	std::lock_guard lk{ mtx };
+	std::lock_guard lg{ mtx };
 
 	// ... production code
 
@@ -19,7 +19,6 @@ void producer()
 
 void consumer()
 {
-	using namespace std::literals;
 	std::unique_lock ulock{ mtx };
 	while (shared_variable == 0)
 	{
@@ -28,14 +27,12 @@ void consumer()
 		std::this_thread::sleep_for(1000ms);
 		ulock.lock();
 	}
-	
+
 	std::cout << "the value is : " << shared_variable << '\n';
 }
 
 int main()
 {
-	std::thread t1{ producer };
-	std::thread t2{ consumer };
-	t1.join();
-	t2.join();
+	std::jthread t1{ producer };
+	std::jthread t2{ consumer };
 }
