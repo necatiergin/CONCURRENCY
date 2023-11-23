9128dfbaf53664d3ca69d5b79774bb56e16f252d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-23 19:12:45 +0300

    Update future_status_01.cpp

--- future/status/future_status_01.cpp
+++ future/status/future_status_01.cpp
@@ -9,21 +9,18 @@ void func(std::promise<int> x)
 	x.set_value(25);
 }
 
-
 int main()
 {
 	std::promise<int> prom;
-	std::future<int> ftr = prom.get_future();
-	std::thread tx{ func,std::move(prom) };
-	std::future_status status{};
+	auto ftr = prom.get_future();
+	std::jthread tx{ func, std::move(prom) };
 
+	std::future_status status{};
 	do {
 		status = ftr.wait_for(200ms);
-
 		std::cout << "Some work ...\n";
 
 	} while (status != std::future_status::ready);
 
 	std::cout << "value is : " << ftr.get() << '\n';
-	tx.join();
 }
