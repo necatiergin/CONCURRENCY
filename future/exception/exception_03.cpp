0145382a26096ce8e728b5704a36f36e53240f82
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-23 19:08:30 +0300

    Update exception_03.cpp

--- future/exception/exception_03.cpp
+++ future/exception/exception_03.cpp
@@ -1,15 +1,16 @@
 #include <future>
 #include <iostream>
 
+//we can call get fucntion of the future object only once
 int main()
 {
 	std::promise<int> prom;
 	auto ft = prom.get_future();
-	
+
 	prom.set_value(10);
 	auto val = ft.get();
 	std::cout << "val = " << val << '\n';
-	
+
 	try {
 		auto x = ft.get();
 	}
