736f763e85a36210c4183918c36d435c893254ab
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-23 11:35:41 +0300

    Create fp_02.cpp

---
+++ future/fp_02.cpp
@@ -0,0 +1,24 @@
+#include <future>
+#include <thread>
+#include <iostream>
+#include <utility>
+
+double func_compute(double dval) 
+{
+	//...
+	return dval * dval;
+}
+
+void produce(std::promise<double> prms, double dval) 
+{
+	prms.set_value(func_compute(dval));
+}
+
+int main() 
+{
+	std::promise<double> prms;
+	auto ftr = prms.get_future(); 
+	std::jthread producer(produce, std::move(prms), 4.5);
+	std::cout << ftr.get() << '\n';
+	producer.join();
+}
