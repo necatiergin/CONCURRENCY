8b3a462f1e28ba45fae815a6e6116f082a0041ac
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-25 06:23:18 +0300

    Update mutex_12.cpp

--- mutex/lock_guard/mutex_12.cpp
+++ mutex/lock_guard/mutex_12.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 #include <syncstream>
 #include <mutex>
+#include <vector>
 
-int gt{};
-std::mutex m;
+int gt{};		//shared variable
+std::mutex mtx;   
 
 void func(char c)
 {
-	std::lock_guard<std::mutex> myguard{ m };
+	std::lock_guard<std::mutex> myguard{mtx};
 	++gt;
-	std::osyncstream{ std::cout } << c << " " << gt << '\n';
+	std::osyncstream{ std::cout } << c << "   " << gt << '\n';
 }
 
 int main()
 {
 	using namespace std;
-	vector<thread> tvec;
-	
+	vector<jthread> tvec;
+
 	for (char c = 'a'; c <= 'z'; ++c)
 	{
 		tvec.emplace_back(func, c);
-	}
-	//
-	for (auto& t : tvec)
-	{
-		t.join();
 	}
 }
