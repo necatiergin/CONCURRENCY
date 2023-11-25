f330f8629ed5f2f8d58ad54af7f38178f8d71016
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-25 06:30:38 +0300

    Update lock_guard_01.cpp

--- mutex/lock_guard/lock_guard_01.cpp
+++ mutex/lock_guard/lock_guard_01.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include <mutex>
 #include <iostream>
+#include <syncstream>
 
 using namespace std::literals;
 
@@ -8,19 +9,18 @@ std::mutex gmtx;
 
 void func(int n, char c)
 {
-	
 	for (int i = 0; i < n; ++i) {
 		try {
 			//gmtx.lock();
 			std::lock_guard lg{ gmtx };
-			std::cout << c;
-			if ((n + 1) % 7 == 0)
+			std::osyncstream{ std::cout } << c << '\n';
+			if (i % 3 == 0)
 				throw std::runtime_error{ "error in loop!\n" };
 			std::this_thread::sleep_for(50ms);
 			//gmtx.unlock();
 		}
 		catch (const std::exception& ex) {
-			//std::cout << "exception caught: " << ex.what() << '\n';
+			std::osyncstream{ std::cout } << "\nexception caught: " << ex.what() << '\n';
 		}
 	}
 	std::cout << '\n';
@@ -29,11 +29,7 @@ void func(int n, char c)
 
 int main()
 {
-	std::thread t1{ func, 20, 'A'};
-	std::thread t2{ func, 20, 'B'};
-	std::thread t3{ func, 20, 'C'};
-
-	t1.join();
-	t2.join();
-	t3.join();
+	std::jthread t1{ func, 3, 'A' };
+	std::jthread t2{ func, 3, 'B' };
+	std::jthread t3{ func, 3, 'C' };
 }
