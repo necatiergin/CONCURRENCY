4ee8a5583ebb8ef3716923c95236b2e5e34e5928
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-25 07:57:16 +0300

    Update unique_lock_01.cpp

--- mutex/unique_lock/unique_lock_01.cpp
+++ mutex/unique_lock/unique_lock_01.cpp
@@ -1,19 +1,29 @@
+#include <mutex>
+
 std::mutex mtx;
 
-void func()
+void f1()
+{
+	std::unique_lock<std::mutex> lock(mtx);
+}
+
+void f2()
 {
 	mtx.lock();
 	std::unique_lock<std::mutex> lock(mtx, std::adopt_lock);
 }
 
-void func()
+void f3()
 {
 	std::unique_lock<std::mutex> ulock(mtx, std::defer_lock);
 
 	ulock.lock();
 }
 
-void func()
+void f4()
 {
 	std::unique_lock<std::mutex> ulock(mtx, std::try_to_lock);
+	if (ulock.owns_lock()) {
+		//...
+	}
 }
