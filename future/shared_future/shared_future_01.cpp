05820452b7c8da1bc39349ca7f47c45471b0e29a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-26 06:06:22 +0300

    Update shared_future_01.cpp

--- future/shared_future/shared_future_01.cpp
+++ future/shared_future/shared_future_01.cpp
@@ -5,7 +5,7 @@
 #include <syncstream>
 
 struct SumSquare {
-	void operator()(std::promise<int>&& prom, int a, int b) 
+	void operator()(std::promise<int>&& prom, int a, int b)
 	{
 		prom.set_value(a * a + b * b);
 	}
@@ -13,29 +13,21 @@ struct SumSquare {
 
 void func(std::shared_future<int> sftr)
 {
-		std::osyncstream os{ std::cout };
-		os << "threadId(" << std::this_thread::get_id() << "): ";
-		os << "result = " << sftr.get() << std::endl;
+	std::osyncstream os{ std::cout };
+	os << "threadId (" << std::this_thread::get_id() << "): ";
+	os << "result = " << sftr.get() << '\n';
 }
 
-int main() 
+int main()
 {
 	std::promise<int> prom;
-	std::shared_future<int> sftr = prom.get_future();
+	std::shared_future sftr = prom.get_future();
 
-	std::thread th(SumSquare{}, std::move(prom), 5, 9);
-	
-	std::thread t1(func, sftr);
-	std::thread t2(func, sftr);
-	std::thread t3(func, sftr);
-	std::thread t4(func, sftr);
-	std::thread t5(func, sftr);
-	
-	th.join();
-	
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
-	t5.join();
+	std::jthread th(SumSquare{}, std::move(prom), 5, 9);
+
+	std::jthread t1(func, sftr);
+	std::jthread t2(func, sftr);
+	std::jthread t3(func, sftr);
+	std::jthread t4(func, sftr);
+	std::jthread t5(func, sftr);
 }
