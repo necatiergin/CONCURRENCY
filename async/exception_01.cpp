afad64b0513663e8c1e98cef390da2907eb6f932
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-26 05:08:58 +0300

    Create exception_01.cpp

---
+++ async/exception_01.cpp
@@ -0,0 +1,36 @@
+//Example: Futures and Exceptions
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <future>
+#include <stdexcept>
+
+double square_root(double x) 
+{
+	if (x < 0.0) {
+		throw std::domain_error("negative value to square root function");
+	}
+	return std::sqrt(x);
+}
+
+
+
+int main() 
+{
+	
+	std::vector dvec{ 2.3, 3.4, 1.1, -1.9 };
+	std::vector<std::future<double>> results;
+	
+	for (auto x : dvec) {
+		results.push_back(std::async(square_root, x));
+	}
+	
+	for (auto& x : results) {
+		try {
+			std::cout << x.get() << '\n';
+		}
+		catch (const std::domain_error&) {
+			std::cout << "error\n";
+		}
+	}
+}
