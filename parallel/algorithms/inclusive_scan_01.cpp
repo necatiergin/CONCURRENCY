15a8409e0db51f017501d12acca407899dd8a04a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-09 07:01:37 +0300

    Create inclusive_scan_01.cpp

---
+++ parallel/algorithms/inclusive_scan_01.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <execution>
+
+int main()
+{
+	using namespace std;
+
+	vector svec{ 1, 3, 5, 7, 9, 11, 13, 15 };
+	vector<int> dvec(svec.size());
+
+	inclusive_scan(std::execution::par, svec.begin(), svec.end(), dvec.begin());
+
+	for (auto i : dvec)
+		cout << i << ' ';
+}
