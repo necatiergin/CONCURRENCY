6f36f5bcdbbb7ee3beacae351f57c043bd9fc2b2
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-09 04:38:27 +0300

    Update parallel_05.cpp

--- parallel/parallel_05.cpp
+++ parallel/parallel_05.cpp
@@ -11,13 +11,15 @@ constexpr std::size_t n{ 20'000'000 };
 
 int main()
 {
-	std::vector<int> vec(n);
+	std::vector<double> vec(n);
 	std::mt19937 eng{ std::random_device{}() };
-	std::uniform_int_distribution dist{ 0, 999'999 };
-	std::generate_n(vec.begin(), n, [&] {return dist(eng); });
+	std::uniform_real_distribution dist{ -20., 20.};
+	std::generate(vec.begin(), vec.end(), [&] {return dist(eng); });
 	auto tp_start = std::chrono::steady_clock::now();
-	sort(std::execution::seq, vec.begin(), vec.end());
+	//sort(std::execution::seq, vec.begin(), vec.end());
+	//sort(std::execution::unseq, vec.begin(), vec.end());
+	//sort(std::execution::par, vec.begin(), vec.end());
+	//sort(std::execution::par_unseq, vec.begin(), vec.end());
 	auto tp_end = std::chrono::steady_clock::now();
-	std::cout << dsec{ tp_end - tp_start }.count() << "saniye\n";
+	std::cout << dsec{ tp_end - tp_start }.count() << " saniye\n";
 }
-
