b606c2295ac9ff6cdb8479fe088f0f676b5f7cdf
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-09 06:09:55 +0300

    Create accummulate_02.cpp

---
+++ parallel/reduce/accummulate_02.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <string>
+#include <execution>
+
+int main()
+{
+	using namespace std;
+
+    vector<int> ivec{ 1, 3, 5, 7, 9 };
+
+    cout << accumulate(ivec.begin(), ivec.end(), 0) << '\n';
+    cout << accumulate(ivec.begin(), ivec.end(), 0, [](int prev_result, int val) { return prev_result + val; }) << '\n';
+    cout << accumulate(ivec.begin(), ivec.end(), ""s, [](string prev_result, int val) { return prev_result + '-' + to_string(val); }) << '\n';
+    //cout << reduce(execution::par, ivec.begin(), ivec.end(), "0"s, [](string prev_result, int val) { return prev_result + to_string(val); }) << '\n';
+}
