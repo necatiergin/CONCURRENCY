b737801dd756d697e2b38175c6cb3b57f9e70565
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-09 07:19:07 +0300

    Create inner_product_01.cpp

---
+++ parallel/algorithms/inner_product_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+int main()
+{
+    std::vector<int> x{ 2, 3, 1, 5, 6 };
+    std::vector<int> y{ 1, 2, 4, 3, 5 };
+    //                  2  6  4  15 30
+
+    auto result = std::inner_product(x.begin(), x.end(), y.begin(), 0);
+    std::cout << "Inner product of x and y: " << result << '\n';
+}
