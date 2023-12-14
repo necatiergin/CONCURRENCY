097407ea174cd830edddc8d34f26420f3b88773d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-15 01:19:06 +0300

    Update and rename atomic/cas_02.cpp to atomic/compare_exchange/cas_02.cpp

--- atomic/compare_exchange/cas_02.cpp
+++ atomic/compare_exchange/cas_02.cpp
@@ -9,19 +9,18 @@ int main()
 
 	int a[] = { 1, 3, 5, 7 };
 	atomic<int*> p = a;
-	cout << "*p = " <<  *p << '\n';
+	cout << "*p = " << *p << '\n';
 	p.store(a + 1);
-	cout << "*p = " <<  *p << '\n';
+	cout << "*p = " << *p << '\n';
 	int* q{ a + 1 };
-	cout << "*q = " << *q << "\n";
+	cout << "*q = " << *q << '\n';
 	bool result = p.compare_exchange_strong(q, a + 3);
-	cout << "result = " << result << "\n";
-	cout << "*p = " <<  *p << '\n';
+	cout << "result = " << result << '\n';
+	cout << "*p = " << *p << '\n';
 	result = p.compare_exchange_strong(q, a);
-	cout << "result = " << result << "\n";
-	cout << "*p = " << *p << "\n";
-	cout << "*q = " << *q << "\n";
+	cout << "result = " << result << '\n';
+	cout << "*p = " << *p << '\n';
+	cout << "*q = " << *q << '\n';
 }
-
 
 
