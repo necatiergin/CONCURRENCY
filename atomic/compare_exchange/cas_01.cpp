379310a49a7e89c5112988ea9facf699d20e0ddd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-15 01:15:36 +0300

    Update and rename atomic/cas_01.cpp to atomic/compare_exchange/cas_01.cpp

--- atomic/compare_exchange/cas_01.cpp
+++ atomic/compare_exchange/cas_01.cpp
@@ -2,20 +2,19 @@
 #include <utility>
 #include <atomic>
 
-
 int main()
 {
 	std::atomic<int> x;
 	x.store(5);
 
-	//int expected{ 5 };
-	int expected{ 7 };
+	int expected{ 5 };
+	//int expected{ 7 };
 	int desired{ 95 };
 	auto b = x.compare_exchange_strong(expected, desired);
 	std::cout << std::boolalpha;
 
-	std::cout << "b = " << b << "\n";
-	std::cout << "x = " << x << "\n";
-	std::cout << "expected = " << expected << "\n";
-	std::cout << "desired  = " << desired << "\n";	
+	std::cout << "b = " << b << '\n';
+	std::cout << "x = " << x << '\n';
+	std::cout << "expected = " << expected << '\n';
+	std::cout << "desired  = " << desired << '\n';
 }
