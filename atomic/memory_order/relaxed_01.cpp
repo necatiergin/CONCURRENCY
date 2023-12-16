8a54ef83a43d586e705484d57f8a45109d414dea
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-16 06:37:41 +0300

    Update and rename atomic/memory_order_relaxed_01.cpp to atomic/memory_order/relaxed_01.cpp

--- atomic/memory_order/relaxed_01.cpp
+++ atomic/memory_order/relaxed_01.cpp
@@ -8,21 +8,22 @@ using namespace std;
 std::atomic<bool> x, y, z;
 
 
-void func_1() {
-
+void func_1() 
+{
     x.store(true, std::memory_order_relaxed);
 }
 
-void func_2() 
+void func_2()
 {
     y.store(true, std::memory_order_relaxed);
 }
 
-void func_3() {
+void func_3()
+{
 
     while (!y.load(std::memory_order_relaxed))
         ;
-    
+
     if (x.load(std::memory_order_relaxed)) {
         z = true;
     }
@@ -30,18 +31,15 @@ void func_3() {
     assert(z);
 }
 
-int main() 
+int main()
 {
-    for (int i = 0; i < 10000; ++i) {
+    for (int i = 0; i < 100000; ++i) {
         x = false;
         y = false;
         z = false;
-        std::thread thread_1(func_1);
-        std::thread thread_2(func_2);
-        std::thread thread_3(func_3);
-        thread_1.join();
-        thread_2.join();
-        thread_3.join();
+        std::jthread thread_1(func_1);
+        std::jthread thread_2(func_2);
+        std::jthread thread_3(func_3);
     }
 
 }
