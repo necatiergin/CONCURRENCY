654d914a1086c340b4f664360df145c9be8bf5a0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-16 07:48:32 +0300

    Update sync_02.cpp

--- atomic/synchronisation/sync_02.cpp
+++ atomic/synchronisation/sync_02.cpp
@@ -1,1 +1,33 @@
 
+#include <condition_variable>
+#include <iostream>
+#include <thread>
+#include <string>
+
+std::string shared;
+
+std::atomic ready{ false };
+
+void consumer() 
+{
+	while (!ready)
+		;
+
+	std::cout << "shared is : " << shared << '\n';
+
+}
+
+void producer() 
+{
+	shared = "necati ergin";
+	ready = true;
+}
+
+int main() 
+{
+	{
+		std::jthread t1(producer);
+		std::jthread t2(consumer);
+	}
+
+}
