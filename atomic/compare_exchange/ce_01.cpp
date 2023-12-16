68de678a1c44b012aa938cc14edb1aa1494f1224
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-16 08:53:08 +0300

    Update ce_01.cpp

--- atomic/compare_exchange/ce_01.cpp
+++ atomic/compare_exchange/ce_01.cpp
@@ -8,17 +8,17 @@ int main()
 	cout << boolalpha;
 	atomic<int> a;
 
-	cout << "a = " << a << "\n";
-	cout << "a.load() = " << a.load() << "\n";
+	cout << "a = " << a << '\n';
+	cout << "a.load() = " << a.load() << '\n';
 	a.store(10);
-	cout << "a = " << a << "\n";
+	cout << "a = " << a << '\n';
 	int expected = 20;
-	cout << "expected = " << expected << "\n";
+	cout << "expected = " << expected << '\n';
 	bool result = a.compare_exchange_strong(expected, 50);
 	// a has not the expected value and will not be set
-	cout << "a = " << a << "\n";
+	cout << "a = " << a << '\n';
 	//result will be false
-	cout << "result = " << result << "\n";
+	cout << "result = " << result << '\n';
 	//expected will be set to the value of a
-	cout << "expected = " << expected << "\n";
+	cout << "expected = " << expected << '\n';
 }
