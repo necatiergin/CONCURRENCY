2b02fff8633b9093a70bb12d0f9709a905ec001d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-20 13:55:20 +0300

    Create free_functions_01.cpp

---
+++ atomic/basic/free_functions_01.cpp
@@ -0,0 +1,15 @@
+#include <atomic>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	atomic<int> x{};
+
+	x.store(98);
+	cout << x.load() << '\n';
+	atomic_store(&x, 30);
+	cout << atomic_load(&x) << '\n';
+	//
+}	
