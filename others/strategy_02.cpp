cdee9c6def201d146d20036a758387bfff1716fb
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-22 12:41:59 +0300

    Create strategy_02.cpp

---
+++ others/strategy_02.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <mutex>
+#include <shared_mutex>
+
+class ILock {
+public:
+	virtual void lock() const = 0;
+	virtual void unlock() const = 0;
+};
+
+
+class StrategizedLocking {
+public:
+	StrategizedLocking(ILock &lock) : mlock(lock) 
+	{
+		mlock.lock();
+	}
+
+	~StrategizedLocking() 
+	{
+		mlock.unlock();
+	}
+private:
+	ILock &mlock;
+};
+
+struct NullObjectMutex {
+	static void lock() {}
+	static void unlock() {}
+};
+
+class NoLock : public ILock {
+	void lock() const override 
+	{
+		std::cout << "NoLock::lock()\n";
+		NullObjectMutex::lock();
+	}
+	
+	void unlock() const override 
+	{
+		std::cout << "NoLock::unlock()\n";
+		NullObjectMutex::unlock();
+	}
+};
+
+class ExclusiveLock : public ILock {
+	void lock() const override 
+	{
+		std::cout << " ExclusiveLock::mlock()\n";
+		mtx.lock();
+	}
+
+	void unlock() const override 
+	{
+		std::cout << " ExclusiveLock::unlock()\n";
+		mtx.unlock();
+	}
+	mutable std::mutex mtx;
+};
+
+
+class SharedLock : public ILock {
+
+	void lock() const override 
+	{
+		std::cout << " SharedLock::lock_shared()\n";
+		ms_mtx.lock_shared();
+	}
+
+	void unlock() const override 
+	{
+		std::cout << " SharedLock::unlock_shared()\n";
+		ms_mtx.unlock_shared();
+	}
+
+	mutable std::shared_mutex ms_mtx;
+};
+
+
+int main() 
+{
+	NoLock no_lock;
+	StrategizedLocking st_lock1{ no_lock };
+	{
+		ExclusiveLock elock;
+		StrategizedLocking st_lock2{elock};
+		{
+			SharedLock shared_lock;
+			StrategizedLocking st_lock3{ shared_lock };
+		}
+	}
+}
