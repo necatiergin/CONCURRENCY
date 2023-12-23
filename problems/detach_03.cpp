dea14f7e1724ffa696b8f4920d036b675523b6d3
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-23 08:01:26 +0300

    Create detach_03.cpp

---
+++ problems/detach_03.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <syncstream>
+
+using namespace std;
+
+void foo() 
+{
+	const string slocal("local string");
+	thread th1([slocal] {
+		osyncstream{ cout } << "by copy capture : " << slocal << '\n';
+	});
+
+	thread th2([&slocal] {
+		osyncstream{ cout } << "by reference capture : " << slocal << '\n';
+	});
+	th1.detach();
+	th2.detach();
+}
+
+
+int main() 
+{
+	using namespace std::literals;
+	foo();
+
+	std::this_thread::sleep_for(2s);
+}
