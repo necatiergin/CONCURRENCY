a3d00cb17de55767a56fdde34fb4f90d39477a74
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-01-03 18:30:06 +0300

    Update constructors_02.cpp

--- thread/constructors/constructors_02.cpp
+++ thread/constructors/constructors_02.cpp
@@ -1,22 +1,23 @@
+#include <syncstream>
 #include <iostream>
 #include <thread>
 
 void foo(int x)
 {
-	std::cout << "in foo x = " << x << '\n';
+	std::osyncstream{ std::cout } << "in foo x = " << x << '\n';
 }
 
 void bar(int x, int y, int z)
 {
-	std::cout << "in bar x = " << x << '\n';
-	std::cout << "in bar y = " << y << '\n';
-	std::cout << "in bar z = " << z << '\n';
+	std::osyncstream{ std::cout} << "in bar x = " << x << '\n';
+	std::osyncstream{ std::cout } << "in bar y = " << y << '\n';
+	std::osyncstream{ std::cout } << "in bar z = " << z << '\n';
 }
 
 int main()
 {
-	std::thread tx{foo, 1};
-	std::thread ty {bar, 10, 20, 30};
+	std::thread tx{ foo, 1 };
+	std::thread ty{ bar, 10, 20, 30 };
 
 	tx.join();
 	ty.join();
