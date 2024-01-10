84c885ca9703ab7db503bff55dfc0882ee73b358
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-01-10 18:15:25 +0300

    Update unique_lock_02.cpp

--- mutex/unique_lock/unique_lock_02.cpp
+++ mutex/unique_lock/unique_lock_02.cpp
@@ -6,6 +6,8 @@ std::timed_mutex mtx;
 void func()
 {
 	using namespace std::literals;
-	std::unique_lock<std::timed_mutex> ulock(mtx, 500ms);
+	//std::unique_lock<std::timed_mutex> ulock(mtx, 500ms);
+	
+	std::unique_lock ulock(mtx, 500ms);
 	//...
 }
