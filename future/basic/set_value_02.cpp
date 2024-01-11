197df0023110e2656aa97d54aabe72f4301fd312
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-01-11 10:28:01 +0300

    Create set_value_02.cpp

---
+++ future/basic/set_value_02.cpp
@@ -0,0 +1,18 @@
+#include <future>
+#include <iostream>
+#include <thread>
+
+int foo(int x)
+{
+	//code
+	return x * x + 5;
+}
+
+int main()
+{
+	std::promise<int> prom;
+	auto ft = prom.get_future();
+	std::jthread tx{ [&](int val) {prom.set_value(foo(val)); }, 20 };
+
+	std::cout << ft.get() << '\n';
+}
