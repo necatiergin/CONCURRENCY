36db6221c12fd286332201c86299eef6dce905f1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-07-05 12:04:57 +0300

    Update sleep_for_01.cpp

--- thread/this_thread/sleep_for_01.cpp
+++ thread/this_thread/sleep_for_01.cpp
@@ -6,14 +6,14 @@ void tfunc(int ms)
 	using namespace std::chrono;
 
 	std::this_thread::sleep_for(milliseconds{ ms });
-	std::cout << "thread id = " << std::this_thread::get_id() << "\n";
+	std::cout << "thread id = " << std::this_thread::get_id() << '\n';
 }
 
 
 int main()
 {
-	std::thread th{ tfunc, 2000 };
-	std::cout << th.get_id() << "\n";
+	std::thread th{ tfunc, 2'000 };
+	std::cout << th.get_id() << '\n';
 	th.join();
 	std::cout << "program sonlaniyor\n";
 }
