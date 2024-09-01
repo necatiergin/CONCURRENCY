5c61ac061dac856ca4461a13901baa3ef175ac9c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-01 11:58:54 +0300

    Create function_argument_01.cpp

---
+++ future/basic/function_argument_01.cpp
@@ -0,0 +1,18 @@
+#include <future>
+#include <functional>
+#include <thread>
+
+void foo(std::promise<int>);
+void bar(std::promise<int>&);
+void baz(std::promise<int>&&);
+
+int main(void)
+{
+	std::promise<int> pr;
+
+	//std::thread t1(foo, pr);  //error
+	std::thread t2(foo, std::move(pr)); //valid
+	//std::thread t3(bar, pr); // error
+	std::thread t4(bar, std::ref(pr));	//valid
+	std::thread t5(baz, std::move(pr));	  //valid
+}
