8b65fa1a13866bca9e16ee8e1761c1fc77344f2c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-01 17:59:28 +0300

    Create jt_08.cpp

---
+++ thread/jthread/jt_08.cpp
@@ -0,0 +1,23 @@
+// from cppreference.com
+
+#include <iostream>
+#include <thread>
+
+using namespace std::literals::chrono_literals;
+
+void f(std::stop_token stop_token, int value)
+{
+    while (!stop_token.stop_requested())
+    {
+        std::cout << value++ << ' ' << std::flush;
+        std::this_thread::sleep_for(200ms);
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::jthread thread(f, 5); // prints 5 6 7 8... for approximately 3 seconds
+    std::this_thread::sleep_for(3s);
+    // The destructor of jthread calls request_stop() and join().
+}
