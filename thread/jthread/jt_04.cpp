4f2d6332396cb4b46a48d10a4162d9b8d7596749
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-01 19:23:14 +0300

    Create jt_04.cpp

---
+++ thread/jthread/jt_04.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <thread>
+
+using namespace std::literals::chrono_literals;
+
+void f(std::stop_token stop_token)
+{
+    while (!stop_token.stop_requested())
+    {
+        std::cout.put('.');
+        std::this_thread::sleep_for(10ms);
+    }
+    std::cout << "\n f is still running!\n";
+}
+
+int main()
+{
+    std::jthread jt(f); // 
+    std::this_thread::sleep_for(5s);
+
+    jt.request_stop();
+    
+}
