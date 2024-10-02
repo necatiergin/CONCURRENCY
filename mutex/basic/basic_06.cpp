0f3640ba7be8c6badb31531763c41fe1064f3921
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-02 15:37:23 +0300

    Update and rename try_lock_01.cpp to basic_06.cpp

--- mutex/basic/basic_06.cpp
+++ mutex/basic/basic_06.cpp
@@ -1,15 +1,16 @@
+// std::mutex::try_lock
+
 #include <iostream>
 #include <thread>
 #include <mutex>
 
-int counter{};					
+int counter{};
 std::mutex counter_mtx;
 
 void try_increase()
 {
 	for (int i = 0; i < 100'000; ++i) {
-		if (counter_mtx.try_lock()) {   
-										
+		if (counter_mtx.try_lock()) {
 			++counter;
 			counter_mtx.unlock();
 		}
@@ -18,16 +19,16 @@ void try_increase()
 
 int main()
 {
-	std::thread ar_t[10]; 
+	std::thread ar_t[10];
 	for (int i = 0; i < 10; ++i)
 		ar_t[i] = std::thread(try_increase);
 
 	for (auto& t : ar_t)
 		t.join();
 
-	std::cout << counter << " kez arttirma islemi yapilabildi.\n";
+	std::cout << counter << " times incremented.\n";
 }
 
-Output
-------
-78718 kez arttirma islemi yapilabildi.
+/*
+	Sample output: 78718 times incremented
+*/
