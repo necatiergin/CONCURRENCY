0caddb03cb713bf28ba2f58f1e0d6df5d7a62796
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-03 18:07:38 +0300

    Create exception_01.cpp

---
+++ thread/exception/exception_01.cpp
@@ -0,0 +1,29 @@
+#include <thread>
+#include <syncstream>
+#include <iostream>
+
+void func(int x, char c)
+{
+	std::osyncstream(std::cout) << "func(int x) x = " << x << '\n';
+
+	try {
+		if (x % 2 == 0)
+			throw std::runtime_error("even number error!");
+	}
+	catch (const std::exception& ex) {
+		std::osyncstream(std::cout) << "\nexception caught: " << ex.what() << '\n';
+	}
+	std::this_thread::sleep_for(std::chrono::duration<double>{2.});
+	for (int i = 0; i < x; ++i) {
+		std::cout.put(c);
+	}
+}
+
+int main()
+{
+	std::thread t1(func, 10000, '*');
+	std::thread t2(func, 14000, '.');
+
+	t1.join();
+	t2.join();
+}
