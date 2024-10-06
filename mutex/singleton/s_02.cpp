2d811d38345a860638c046672c4d81ac0eb6ecc4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-06 10:52:14 +0300

    Create s_02.cpp

---
+++ mutex/singleton/s_02.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <syncstream>
+#include <thread>
+#include <vector>
+
+class ThreadSafeSingleton {
+	inline static ThreadSafeSingleton* mp{};
+	
+	ThreadSafeSingleton() 
+	{ 
+		std::osyncstream{ std::cout } << "Initializing Singleton\n";
+	}
+public:
+	
+	ThreadSafeSingleton(const ThreadSafeSingleton&) = delete;
+	ThreadSafeSingleton& operator=(const ThreadSafeSingleton&) = delete;
+
+	static ThreadSafeSingleton& get_Singleton()
+	{ 
+		static ThreadSafeSingleton s;
+
+		return s;
+	}
+};
+
+void foo()
+{
+	ThreadSafeSingleton& rs = ThreadSafeSingleton::get_Singleton();
+	std::osyncstream{ std::cout } << &rs << '\n';
+}
+
+int main()
+{
+	std::vector<std::thread> tvec;
+
+	for (int i = 0; i < 20; ++i)
+		tvec.push_back(std::thread(foo));
+
+	for (auto& th : tvec)
+		th.join();
+}
