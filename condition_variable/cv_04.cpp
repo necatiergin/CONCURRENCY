eb812a14b2d7e605683dd26dd1bb54cc3420c784
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-07 15:56:10 +0300

    Update cv_04.cpp

--- condition_variable/cv_04.cpp
+++ condition_variable/cv_04.cpp
@@ -5,48 +5,47 @@
 #include <iomanip>
 
 
-using namespace std;
-using namespace chrono;
+std::string	shared_data;
+std::mutex	mtx;
+std::condition_variable cv;
+bool cflag{ false };
 
-string				shared_data;
-mutex				mtx;
-condition_variable	        cv;
-bool				cflag{ false };
+using namespace std::chrono_literals;
 
 void reader()
 {
-	cout << "READER thread is locking the mutex\n";
-	unique_lock ulock(mtx);
+	std::cout << "READER thread is locking the mutex\n";
+	std::unique_lock ulock(mtx);
 
-	cout << "READER thread has locked the mutex\n";
-	cout << "READER thread is going to sleep...\n";
+	std::cout << "READER thread has locked the mutex\n";
+	std::cout << "READER thread is going to sleep...\n";
 
 	cv.wait(ulock, [] {return cflag; });
 
-	cout << "READER thread wakes up\n";
-
-	cout << quoted(shared_data) << '\n';
-	cout << "Reader thread unlocks the mutex\n";
+	std::cout << "READER thread wakes up\n";
+	std::cout << quoted(shared_data) << '\n';
+	std::cout << "Reader thread unlocks the mutex\n";
 }
 
 void writer()
 {
+
 	{
-		cout << "WRITER thread is locking the mutex\n";
+		std::cout << "WRITER thread is locking the mutex\n";
 
-		scoped_lock slock(mtx);
-		cout << "WRITER thread has locked the mutex\n";
+		std::scoped_lock slock(mtx);
+		std::cout << "WRITER thread has locked the mutex\n";
 
-		this_thread::sleep_for(2s);
+		std::this_thread::sleep_for(2s);
 
 		// Modify the string
-		cout << "WRITER thread modifying data...\n";
+		std::cout << "WRITER thread modifying data...\n";
 		shared_data = "shared data is ready now";
 		cflag = true;
-		cout << "WRITER thread unlocks the mutex\n";
+		std::cout << "WRITER thread unlocks the mutex\n";
 	}
 
-	cout << "WRITER thread is sending a notification\n";
+	std::cout << "WRITER thread is sending a notification\n";
 	cv.notify_one();
 }
 
@@ -54,9 +53,9 @@ int main()
 {
 	shared_data = "not ready yet.";
 
-	cout << "shared_data value is : " << quoted(shared_data) << '\n';
+	std::cout << "shared_data value is : " << quoted(shared_data) << '\n';
 
-	jthread twriter(writer);
-	this_thread::sleep_for(500ms);
-	jthread treader(reader);
+	std::jthread twriter(writer);
+	std::this_thread::sleep_for(500ms);
+	std::jthread treader(reader);
 }
