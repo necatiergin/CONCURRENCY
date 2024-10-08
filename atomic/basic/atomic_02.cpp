f132de251ef40e0953301fc157dec1a16c24d12a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-08 15:06:07 +0300

    Update atomic_02.cpp

--- atomic/basic/atomic_02.cpp
+++ atomic/basic/atomic_02.cpp
@@ -3,14 +3,12 @@
 
 int main()
 {
-	using namespace std;
-
-	atomic x = 5;
-	atomic y = 5;
+	std::atomic x = 5;
+	std::atomic y = 5;
 
 	auto r1 = x += 2;
 	auto r2 = y.fetch_add(2);
 
-	cout << "r1 = " << r1 << '\n'; //7
-	cout << "r2 = " << r2 << '\n'; //5
+	std::cout << "r1 = " << r1 << '\n'; //7
+	std::cout << "r2 = " << r2 << '\n'; //5
 }
