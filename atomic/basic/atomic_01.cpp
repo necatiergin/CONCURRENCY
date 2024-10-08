55de66dbef5644e8a1d2ee6d4a20c6eea21e1586
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-08 15:04:05 +0300

    Update atomic_01.cpp

--- atomic/basic/atomic_01.cpp
+++ atomic/basic/atomic_01.cpp
@@ -8,8 +8,8 @@ int main()
 	cout << boolalpha;
 	atomic<bool> flag_1;
 	atomic<bool> flag_2; //indetermined value before before C++20. false value since C++20
-	cout << flag_1 << '\n';
-	cout << flag_2 << '\n';
+	cout << "flag_1 = " << flag_1 << '\n';
+	cout << "flag_2 = " << flag_1 << '\n';
 	//atomic<bool> flag_3{flag_2}; //invalid
 	//flag_1 = flag_2; //invalid
 
@@ -21,8 +21,8 @@ int main()
 	cout << "flag_1 = " << flag_1 << '\n';  // operator T
 	cout << "flag_2 = " << flag_2 << '\n';  // operator T
 
-	auto b = flag_1.exchange(true);
-	cout << "b = " << b << '\n';
+	auto b_old = flag_1.exchange(true);
+	cout << "b_old = " << b_old << '\n';
 	cout << "flag_1 = " << flag_1 << '\n';  // operator T
 	cout << "flag_1.load() = " << flag_1.load() << '\n';
 	cout << "flag_2.load() = " << flag_2.load() << '\n';
