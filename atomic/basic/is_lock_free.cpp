dd8faecbe1700237c651d4ca2abf64e73f3f11ab
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-08 15:17:05 +0300

    Update is_lock_free.cpp

--- atomic/basic/is_lock_free.cpp
+++ atomic/basic/is_lock_free.cpp
@@ -4,13 +4,11 @@
 
 int main()
 {
-	using namespace std;
-
-	boolalpha(cout);
-	atomic<int> a1;
-	cout << "a1.lock_free() : " << a1.is_lock_free() << '\n';
-	atomic<long long> a2;
-	cout << "a2.lock_free() : " << a2.is_lock_free() << '\n';
-	atomic<shared_ptr<int>> a3; //C++20
-	cout << "a3.lock_free() : " << a3.is_lock_free() << '\n';
+	boolalpha(std::cout);
+	std::atomic<int> a1;
+	std::cout << "a1.lock_free() : " << a1.is_lock_free() << '\n';
+	std::atomic<long long> a2;
+	std::cout << "a2.lock_free() : " << a2.is_lock_free() << '\n';
+	std::atomic<std::shared_ptr<int>> a3; //C++20
+	std::cout << "a3.lock_free() : " << a3.is_lock_free() << '\n';
 }
