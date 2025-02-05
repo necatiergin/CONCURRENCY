358f03fe92f99a836be08d6ec3ff8a819344ba11
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-02-05 16:38:39 +0300

    Update thread_local_05.cpp

--- thread/thread_local/thread_local_05.cpp
+++ thread/thread_local/thread_local_05.cpp
@@ -21,22 +21,19 @@ void foo(int x)
 
 void func(int x)
 {
-	std::osyncstream{ std::cout } << "func(int) cagrildi x = " << x << '\n';
+	std::osyncstream{ std::cout } << "func(int) called x = " << x << '\n';
 	foo(x);
 }
 
 int main()
 {
 
-	std::thread t1{ func, 1 };
-	std::thread t2{ func, 2 };
-	std::thread t3{ func, 3 };
-	std::thread t4{ func, 4 };
-
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
+	{
+		std::jthread t1{ func, 1 };
+		std::jthread t2{ func, 2 };
+		std::jthread t3{ func, 3 };
+		std::jthread t4{ func, 4 };
+	}
 
 	std::cout << "main is still running...\n";
 }
