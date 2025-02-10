f83590d5bd6f3df678be53b2e3fe8d54d4647ed9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-02-10 09:35:33 +0300

    Update lock_guard_04.cpp

--- mutex/lock_guard/lock_guard_04.cpp
+++ mutex/lock_guard/lock_guard_04.cpp
@@ -5,23 +5,35 @@
 unsigned long long counter = 0;
 std::mutex mtx;
 
-void func()
+void foo()
 {
 	for (unsigned long long i = 0; i < 1'000'000ull; ++i) {
 		mtx.lock();
-		std::lock_guard<std::mutex> lg(mtx, std::adopt_lock);
+		std::lock_guard lg(mtx, std::adopt_lock);
 		++counter;
 	}
 }
+
+void bar()
+{
+	for (unsigned long long i = 0; i < 1'000'000ull; ++i) {
+		std::lock_guard lg(mtx);
+		++counter;
+	}
+}
+
 int main()
 {
 	{
-		std::jthread t1(func);
-		std::jthread t2(func);
-		std::jthread t3(func);
-		std::jthread t4(func);
+		std::jthread t1(foo);
+		std::jthread t2(foo);
+		std::jthread t3(foo);
+		std::jthread t4(foo);
+		std::jthread t5(bar);
+		std::jthread t6(bar);
+		std::jthread t7(bar);
+		std::jthread t8(bar);
 	}
-	
+
 	std::cout << "counter = " << counter << '\n';
 }
-
