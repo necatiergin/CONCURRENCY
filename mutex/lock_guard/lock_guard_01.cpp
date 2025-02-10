7b9af0b68a19cc73e31b483b357d91a8c4f8377b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-02-10 09:21:03 +0300

    Update lock_guard_01.cpp

--- mutex/lock_guard/lock_guard_01.cpp
+++ mutex/lock_guard/lock_guard_01.cpp
@@ -1,32 +1,32 @@
+// deadlock & exception
+
 #include <chrono>
 #include <mutex>
 #include <iostream>
 #include <syncstream>
 
-using namespace std;
-using namespace std::literals;
-
 std::mutex gmtx;
-
 
 void func(int x)
 {
+	std::osyncstream ocout{ std::cout };
+
 	gmtx.lock();
 	try {
-		osyncstream{ std::cout } << this_thread::get_id() << " " << "locked the mutex\n";
-		osyncstream{ std::cout } << "x = " << x << '\n';
+		ocout << std::this_thread::get_id() << " " << "locked the mutex\n";
+		ocout << "x = " << x << '\n';
 		if (x % 2 == 0)
-			throw invalid_argument{ "no even number" };
+			throw std::invalid_argument{ "no even number" };
 		gmtx.unlock();
-		osyncstream{ std::cout } << this_thread::get_id() << " " << "unocked the mutex\n";
+		ocout << std::this_thread::get_id() << " " << "unlocked the mutex\n";
 	}
-	catch (const exception& ex) {
-		osyncstream{ std::cout } << this_thread::get_id() << " exception caught : " << ex.what() << '\n';
+	catch (const std::exception& ex) {
+		ocout << std::this_thread::get_id() << " exception caught : " << ex.what() << '\n';
 	}
 }
 
 int main()
 {
-	jthread t1{ func, 4 };
-	jthread t2{ func, 5 };
+	std::jthread t1{ func, 4 };
+	std::jthread t2{ func, 5 };
 }
