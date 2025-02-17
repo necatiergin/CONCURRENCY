8a1316c1054145fc0efd68db66274d71e1c96b4c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-02-17 17:30:57 +0300

    Update future_01.cpp

--- future/future_01.cpp
+++ future/future_01.cpp
@@ -9,13 +9,16 @@ int main()
 	std::promise<int> prom;
 	//std::future<int> ftr = prom.get_future();
 	//std::future ftr = prom.get_future();   // CTAD
-	auto ftr = prom.get_future();   
+	auto ftr = prom.get_future();
 
 	prom.set_value(991);
 	auto val = ftr.get();
 	std::cout << "val = " << val << '\n';
 
 	try {
+		if (!ftr.valid()) {
+			std::cout << "future object is not valid!\n";
+		}
 		val = ftr.get();   // future object is not valid
 	}
 	catch (const std::future_error& ex)
