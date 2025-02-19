cfe785c4029ac0116954026e4debaa4e1077f191
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-02-19 18:01:52 +0300

    Create ptask_001.cpp

---
+++ packaged_task/ptask_001.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <future>
+#include <thread>
+#include <chrono>
+int sum(int a, int b) 
+{
+    std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    return a + b;
+}
+
+int main() 
+{
+    std::packaged_task<int(int, int)> task(sum);
+
+    std::future<int> result = task.get_future();
+
+    std::thread t(std::move(task), 10, 20);
+
+    std::cout << "result is: " << result.get() << '\n';
+
+    t.join();
+}
