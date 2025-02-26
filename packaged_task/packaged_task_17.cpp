c08d3639105375c06ed2f69a179dbd3eb6774873
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-02-26 18:47:45 +0300

    Update packaged_task_17.cpp

--- packaged_task/packaged_task_17.cpp
+++ packaged_task/packaged_task_17.cpp
@@ -2,22 +2,22 @@
 #include <future>
 #include <thread>
 
-int sum(int x, int y) 
+int sum(int x, int y)
 {
     return x + y;
 }
 
-int main() 
+int main()
 {
-    std::packaged_task<int(int, int)> task(sum); 
-    std::future<int> sonuc = task.get_future();
+    std::packaged_task<int(int, int)> task(sum);
+    std::future<int> result = task.get_future();
 
-    task(33, 67); 
-    std::cout << "result : " << sonuc.get() << '\n';
+    task(33, 67);
+    std::cout << "result : " << result.get() << '\n';
 
-    task.reset(); 
-    sonuc = task.get_future(); 
+    task.reset();
+    result = task.get_future();
 
-    std::thread(std::move(task), 99, 101).detach(); 
-    std::cout << "result: " << sonuc.get() << '\n';
+    std::thread(std::move(task), 99, 101).detach();
+    std::cout << "result: " << result.get() << '\n';
 }
