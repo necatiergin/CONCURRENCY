22aacca3899a428b6870fd8ec23e0e5fbb4307e0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-06 21:45:53 +0300

    Update exception_02.cpp

--- thread/exception/exception_02.cpp
+++ thread/exception/exception_02.cpp
@@ -4,27 +4,25 @@
 
 void func(int x, char c)
 {
-	std::osyncstream ocout{ std::cout };
-	
-	ocout << "func(int x) x = " << x << '\n';
+	std::osyncstream { std::cout } << "func(int x) x = " << x << '\n';
 
 	try {
 		if (x % 2 == 0)
 			throw std::runtime_error("even number error!");
 	}
 	catch (const std::exception& ex) {
-		ocout << "\nexception caught: " << ex.what() << '\n';
+		std::osyncstream{ std::cout } << "\nexception caught: " << ex.what() << '\n';
 	}
 	std::this_thread::sleep_for(std::chrono::duration<double>{2.});
 	for (int i = 0; i < x; ++i) {
-		ocout.put(c);
+		std::osyncstream{ std::cout }.put(c);
 	}
 }
 
 int main()
 {
-	std::thread t1(func, 10000, '*');
-	std::thread t2(func, 14000, '.');
+	std::thread t1(func, 100, '*');
+	std::thread t2(func, 140, '.');
 
 	t1.join();
 	t2.join();
