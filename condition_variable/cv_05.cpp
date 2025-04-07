cd45779c0f90515feef48bb68a41d376a57077a8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-07 07:27:34 +0300

    Update cv_05.cpp

--- condition_variable/cv_05.cpp
+++ condition_variable/cv_05.cpp
@@ -2,7 +2,8 @@
 #### lost wakeup
 
 + _condition_variable_ sınıfının _wait_ fonksiyonu sınıfın _notify_ fonksiyonu çağrılana kadar _thread_'i bloke eder.
-+ eğer _writer thread_'de _notify_ fonksiyonu _reader thread_'deki _wait_ çağrısından daha önce gerçekleştirilirse bir sinyal _(notification)_ gönderilmiş olmasına karşın henüz beklemekte olan bir _thread_ yoktur.
++ eğer _writer thread_'de _notify_ fonksiyonu _reader thread_'deki _wait_ çağrısından daha önce gerçekleştirilirse 
++ bir sinyal _(notification)_ gönderilmiş olmasına karşın henüz beklemekte olan bir _thread_ yoktur.
 + bu durumda _reader thread_ hiçbir şekilde uyanamaz ve bloke olarak kalır.
 + bu duruma _lost-wakeup_ denir. 
 */
