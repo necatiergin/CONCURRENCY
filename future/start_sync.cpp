c351d5d4c137065847df2f5148963fda8ab99792
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-14 05:54:30 +0300

    Create start_sync.cpp

---
+++ future/start_sync.cpp
@@ -0,0 +1,21 @@
+#include <future>
+#include <iostream>
+
+void wait_for_go(std::future<void> f) 
+{
+    f.wait(); // start signal is expected from another thread
+    std::cout << "signal received!\n";
+}
+
+int main() 
+{
+    std::promise<void> p;
+    std::future<void> f = p.get_future();
+
+    std::thread t(wait_for_go, std::move(f));
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    p.set_value(); // start signal
+
+    t.join();
+}
