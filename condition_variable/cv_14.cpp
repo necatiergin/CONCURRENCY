df7c781c561f07568275d071ead35692d148c8ef
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-04-20 06:40:38 +0300

    Update cv_14.cpp

--- condition_variable/cv_14.cpp
+++ condition_variable/cv_14.cpp
@@ -8,26 +8,27 @@ std::mutex mtx;
 std::condition_variable cv;
 bool ready = false;
 
-bool predicate() 
+bool predicate()
 {
     std::cout << "[Predicate] Checking condition (ready == " << std::boolalpha << ready << ")\n";
     return ready;
 }
 
-void waiting_thread() {
-    //std::this_thread::sleep_for(std::chrono::milliseconds(300)); // Let notifier go first
-    std::this_thread::sleep_for(std::chrono::milliseconds(0)); // Let notifier go first
+void waiting_thread() 
+{
+    //std::this_thread::sleep_for(std::chrono::milliseconds(300)); 
+    std::this_thread::sleep_for(std::chrono::milliseconds(0)); 
     std::cout << "[WaitingThread] Trying to acquire lock...\n";
 
     std::unique_lock<std::mutex> lock(mtx);
     std::cout << "[WaitingThread] Lock acquired. Calling wait...\n";
 
-    cv.wait(lock, predicate); // predicate() will log internally
+    cv.wait(lock, predicate); 
 
     std::cout << "[WaitingThread] Woken up. Condition met. Continuing work...\n";
 }
 
-void notifier_thread() 
+void notifier_thread()
 {
     std::cout << "[Notifier] Sleeping 100ms before setting condition...\n";
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -42,7 +43,7 @@ void notifier_thread()
     cv.notify_one();
 }
 
-int main() 
+int main()
 {
     std::thread t1(waiting_thread);
     std::thread t2(notifier_thread);
