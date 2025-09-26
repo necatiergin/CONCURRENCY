9dc6eaa78f859a1bf60967f0906026ee18a9410f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-09-26 12:04:42 +0300

    Update and rename stl_inclusive_scan_01.cpp to prl_13.cpp

--- parallel/prl_13.cpp
+++ parallel/prl_13.cpp
@@ -1,11 +1,16 @@
+// inclusive_scan
+
 #include <vector>
 #include <numeric>
 #include <iostream>
 #include <execution>
+#include <print>
 
 int main()
 {
 	std::vector ivec{ 1, 3, 5, 7, 9, 11, 13, 15 };
+	print("{}\n", ivec);
+
 	std::vector<int> dest(ivec.size());
 
 	//inclusive_scan(std::execution::seq, ivec.begin(), ivec.end(), dest.begin());
@@ -13,6 +18,5 @@ int main()
 	//inclusive_scan(std::execution::par_unseq, ivec.begin(), ivec.end(), dest.begin());
 	//inclusive_scan(std::execution::unseq, ivec.begin(), ivec.end(), dest.begin());
 
-	for (auto i : dest)
-		std::cout << i << ' ';
+	print("{}\n", dest);
 }
