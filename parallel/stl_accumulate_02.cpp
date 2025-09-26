35b877b4b1159443f3ad17e6c158e1c8b2ccefff
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-09-26 09:48:38 +0300

    Update stl_accumulate_02.cpp

--- parallel/stl_accumulate_02.cpp
+++ parallel/stl_accumulate_02.cpp
@@ -11,7 +11,13 @@ int main()
     vector<int> ivec{ 1, 3, 5, 7, 9 };
 
     cout << accumulate(ivec.begin(), ivec.end(), 0) << '\n';
-    cout << accumulate(ivec.begin(), ivec.end(), 0, [](int prev_result, int val) { return prev_result + val; }) << '\n';
-    cout << accumulate(ivec.begin(), ivec.end(), ""s, [](string prev_result, int val) { return prev_result + '-' + to_string(val); }) << '\n';
-    //cout << reduce(execution::par, ivec.begin(), ivec.end(), "0"s, [](string prev_result, int val) { return prev_result + to_string(val); }) << '\n';
+    cout << accumulate(ivec.begin(), ivec.end(), 0, 
+        [](int prev_result, int val) { 
+            return prev_result + val; })
+        << '\n';
+    
+    auto fn = [](string prev_result, int val) { return prev_result + '-' + to_string(val); };
+
+    cout << accumulate(ivec.begin(), ivec.end(), ""s, fn) << '\n';
+    cout << reduce(execution::par, ivec.begin(), ivec.end(), ""s, fn) << '\n'; //??
 }
