e8bd6bb8c7130049a3975dd69281f493f6939b6d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-12-11 17:12:50 +0300

    Update get_id_03.cpp

--- thread/members/get_id_03.cpp
+++ thread/members/get_id_03.cpp
@@ -5,21 +5,21 @@ std::thread::id main_thread_id;
 
 void func()
 {
-	//func'ın hangi thread'den çağrıldığına bağlı olarak farklı işler yapılıyor
-	if (std::this_thread::get_id() == main_thread_id) {
-		std::cout << "call from the main thread\n";
-	}
-	else {
-		std::cout << "call from a child thread\n";
-	}
+    // different operations are performed depending on the thread from which func is called
+    if (std::this_thread::get_id() == main_thread_id) {
+        std::cout << "call from the main thread\n";
+    }
+    else {
+        std::cout << "call from a child thread\n";
+    }
 }
 
 int main()
 {
-	main_thread_id = std::this_thread::get_id();
+    main_thread_id = std::this_thread::get_id();
 
-	func();
+    func();
 
-	std::thread t{ func };
-	t.join();
+    std::thread t{ func };
+    t.join();
 }
